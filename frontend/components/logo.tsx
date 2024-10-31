a0a89dce9370981767d4344edcc33c5dd80f0f2a
Brandon Tupiti <tupitibrandon@gmail.com>
2024-10-31 11:56:00 +0000

    add Link to logo

--- frontend/components/logo.tsx
+++ frontend/components/logo.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 import logoDark from "@/assets/bt-dark.svg";
 import logoLight from "@/assets/bt-light.svg";
 
+
 function Logo() {
     return ( 
-        <div className="animate-fadein my-auto font-extrabold text-3xl flex">
+        <Link href={'/'}>
             <Image className="w-16 hidden dark:block" src={logoDark} alt={'logo'}/>            
             <Image className="w-16 dark:hidden" src={logoLight} alt={'logo'}/>            
-        </div>
+        </Link>
      );
 }
 
