17adb01981268f10f58e0e4d6a9e6a23671c54b2
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-07 21:19:36 +0000

    update slug page

--- frontend/app/[slug]/page.tsx
+++ frontend/app/[slug]/page.tsx
@@ -1,20 +1,29 @@
 // importing and assigning blogs data
 import blogsData from '@/posts.json';
-import Container from '@/ui/container';
 import Title from '@/ui/title';
+import Image from 'next/image';
+import { CalendayDaysIcon } from '@/icons/icons';
+
+type PostContent = {
+  contentType: 'paragraph' | 'quote' | 'image' | 'code',
+  content: string,
+}
 
 // duplicate
 type Post = {
-  id: number;
-  slug: string;
-  title: string;
-  readTime: string;
-  content: string;
+  id: number,
+  slug: string,
+  title: string,
+  postImageURL: string,
+  readTime: string,
+  body: PostContent[],
+  date: string,
+  author: string,
 }
-const posts: Post[] = blogsData;
 
 
 
+const posts: Post[] = blogsData as Post[];
 
 export default function Post({ params }: { params: { slug: string } }) {
 
@@ -26,10 +35,56 @@ export default function Post({ params }: { params: { slug: string } }) {
   else {
     return (
       <section className='dark:bg-dark dark:text-white'>
-        <div className="max-w-[720px] m-auto px-16 lg:px-0">
-          <Title>{post.title}</Title>
-          <h2>{post.readTime}</h2>
-          <p>{post.content}</p>
+        <div className="max-w-[720px] m-auto px-16 py-8 lg:py-0 lg:px-0">
+          <div className='flex flex-col gap-6'>
+
+          <div style={{ width: '100%', height: '300px', position: 'relative' }}> {/* Fixed height */}
+            <Image 
+              src={post.postImageURL} 
+              alt="Sample Image"
+              layout="fill"
+              objectFit="cover" // Fills the container, cropping if necessary
+            />
+          </div>
+            
+            <div className='flex flex-col gap-2'>
+              <h4 className='dark:text-slate-200'> By <strong>{post.author}</strong></h4>
+              <h5 className='text-slate-400 flex gap-x-2'>
+                 <CalendayDaysIcon className="size-6" />
+                 {post.date}
+              </h5>
+            </div>
+
+            <div className='mt-8'>
+            <Title>{post.title}</Title>
+            </div>
+         
+            {post.body.map((item, index) => {
+              switch (item.contentType) {
+                case 'paragraph':
+                  return <p className='text-slate-700 dark:text-slate-300 tracking-normal leading-loose' key={index}>{item.content}</p>;
+                case 'quote':
+                  return <blockquote className='p-4 text-lg border-l-4 bg-slate-200 dark:bg-slate-700' key={index}>&ldquo;{item.content}&rdquo;</blockquote>;
+                case 'image':
+                  return <Image 
+                          key={index} 
+                          src={item.content} 
+                          width={200}
+                          height={200}
+                          alt="Post image" 
+                          />
+                          ;
+                case 'code':
+                  return (
+                    <pre key={index}>
+                      <code>{item.content}</code>
+                    </pre>
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </div>
         </div>
       </section>
     )
