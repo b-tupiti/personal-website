20f8ad99b11a12fa041329190440a34a90e06c4e
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-08 22:51:01 +0000

    add Blog page

--- backend/cms/blog/models.py
+++ backend/cms/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Add these:
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class BlogPage(Page):
+
+    date = models.DateField(blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('body'),
+    ]
