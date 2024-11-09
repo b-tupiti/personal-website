e535032167806a3bfc5657b6e4075d0827303afc
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-09 23:21:35 +0000

    update file

--- frontend/app/theme-provider.tsx
+++ frontend/app/theme-provider.tsx
@@ -1,8 +1,6 @@
 "use client"
- 
 import * as React from "react"
 import { ThemeProvider}  from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
  
 export function Providers({ children }: {children: React.ReactNode}) {
   return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
