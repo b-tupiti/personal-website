e2180a60a62ca29e00b85ccd309bc7ddbd9b64da
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-12 17:39:08 +0000

    update body field in BlogPage; set to streamfield

--- backend/cms/blog/models.py
+++ backend/cms/blog/models.py
@@ -1,16 +1,21 @@
 from django.db import models
-from grapple.models import GraphQLString, GraphQLStreamfield
+from grapple.models import GraphQLString, GraphQLStreamfield, GraphQLRichText
 
 # Add these:
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail.fields import StreamField
 from wagtail.admin.panels import FieldPanel
-
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.blocks import RichTextBlock, BlockQuoteBlock
 
 class BlogPage(Page):
 
     date = models.DateField(blank=True)
-    body = RichTextField(blank=True)
+    body = StreamField([
+        ('paragraph', RichTextBlock()),
+        ('quote', BlockQuoteBlock(max_length=100, help_text='quote')),
+        ('image', ImageChooserBlock(required=False)),
+    ])
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
@@ -20,5 +25,5 @@ class BlogPage(Page):
     # add fields to graphQL
     graphql_fields = [
         GraphQLString("date"),
-        GraphQLString("body"),
+        GraphQLStreamfield("body"),
     ]
