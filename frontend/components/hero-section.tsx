5fceaca27a83ac34ef1bae0b15f7d7d5590d475a
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-15 23:53:11 +0000

    Update HeroSection

--- frontend/components/hero-section.tsx
+++ frontend/components/hero-section.tsx
@@ -1,14 +1,15 @@
 import Avatar from "@/components/avatar";
 import Bio from "@/components/bio";
 
-function HeroSection() {
+const HeroSection = () => {
     return (
         <main 
             className="bg-[#fffefe] dark:bg-dark py-8"
             >
-            <div className="max-w-[720px] m-auto">
+            <div 
+                className="max-w-[720px] m-auto">
                 <div 
-                    className="flex flex-col items-center gap-16  lg:flex-row     lg:py-16   "
+                    className="flex flex-col items-center gap-16  lg:flex-row lg:py-16"
                 >
                     <Avatar/>       
                     <Bio/>
