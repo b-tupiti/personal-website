69b1161bd7cad6b5cfde1189144fa4c3ca5ffb1a
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-15 23:34:20 +0000

    Update Title

--- frontend/ui/title.tsx
+++ frontend/ui/title.tsx
@@ -1,7 +1,9 @@
-function Title(props: any) {
+import { ReactNode } from "react";
+
+const Title = ({children}: {children: ReactNode}) => {
     return ( 
         <h1 className="capitalize text-2xl font-bold text-dark dark:text-slate-200">
-            {props.children}
+            {children}
         </h1> 
      );
 }
