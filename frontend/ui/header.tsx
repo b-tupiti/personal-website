1a094d5d066ec1baaa41f2a8676ec7e1ebc787a9
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-15 23:21:23 +0000

    Update Header

--- frontend/ui/header.tsx
+++ frontend/ui/header.tsx
@@ -1,12 +1,13 @@
+import { ReactNode } from "react";
 import Container from "./container";
 import FlexBox from "./flexbox";
 
-function Header(props: any) {
+const Header = ({children}: {children: ReactNode}) => {
     return (  
       <header className=' dark:bg-dark bg-[#fffefe]'>
         <Container>
           <FlexBox twstyle="flex justify-between items-center p-8 lg:px-16"> 
-            {props.children}
+            {children}
           </FlexBox>
         </Container>
       </header>
