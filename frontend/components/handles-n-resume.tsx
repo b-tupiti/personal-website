cfd5cd8ea9cb53886ec9763b8393aa8d642d3da7
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-15 23:52:20 +0000

    Update HandlesAndResume

--- frontend/components/handles-n-resume.tsx
+++ frontend/components/handles-n-resume.tsx
@@ -1,42 +1,41 @@
 import FlexBox from "@/ui/flexbox";
 import Link from "next/link";
 
-const links = [
-      {
-            id: "1",
-            label: "Linkedin",
-            href: "https://google.com",
-      },
-      {
-            id: "2",
-            label: "Github",
-            href: "https://github.com/b-tupiti",
-      },
-      {
-            id: "3",
-            label: "Resume",
-            href: "https://guthib.com",
-      }
-]
+const HandlesAndResume = () => {
+    // TODO: fetch from the backend
+    const links = [
+        {
+              id: "1",
+              label: "Linkedin",
+              href: "https://google.com",
+        },
+        {
+              id: "2",
+              label: "Github",
+              href: "https://github.com/b-tupiti",
+        },
+        {
+              id: "3",
+              label: "Resume",
+              href: "https://guthib.com",
+        }
+    ]
 
-function HandlesAndResume() {
     return (
         <FlexBox 
           twstyle="flex mt-2 lg:mt-0 gap-4 xl:ml-auto xl:mr-8"
           >
             {
-                  links.map(link => (
-                        <Link 
-                            key={link.id}
-                            href={link.href}  
-                            className="capitalize dark:text-slate-400 text-dark text-sm "
-                        >
-                            {link.label}
-                        </Link>
-                    ))
+                links.map(link => (
+                    <Link 
+                        key={link.id}
+                        href={link.href}  
+                        className="capitalize dark:text-slate-400 text-dark text-sm "
+                    >
+                        {link.label}
+                    </Link>
+                ))
             }
-          
-        
         </FlexBox> 
     );
 }
