2b7927961ce7987cf273a4188bc6fed912997eaf
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-15 23:47:03 +0000

    Update Bio

--- frontend/components/bio.tsx
+++ frontend/components/bio.tsx
@@ -1,14 +1,23 @@
 import FlexBox from "@/ui/flexbox";
 
 
-function Bio() {
+const Bio = () => {
+    // TODO: fetch from the backend
+    const name = "Brandon Tupiti";
+    const bio = "Software Developer / UIUX Designer.";
+
     return (
-        <FlexBox twstyle="flex flex-col gap-4 items-center lg:items-start">
-            <h1 className="text-3xl lg:text-5xl dark:text-white text-[#0a1c29]  font-extrabold text-start sm:text-left  w-full">
-                Brandon Tupiti
+        <FlexBox 
+            twstyle="flex flex-col gap-4 items-center lg:items-start">
+            <h1 
+                className="text-3xl lg:text-5xl dark:text-white text-[#0a1c29]  font-extrabold text-start sm:text-left  w-full"
+                >
+                {name}
             </h1>
-            <p className=" dark:text-slate-200 text-dark text-md mt-2">
-            Software Developer / UIUX Designer.
+            <p 
+                className=" dark:text-slate-200 text-dark text-md mt-2"
+                >
+                {bio}
             </p>
         </FlexBox>
     );
