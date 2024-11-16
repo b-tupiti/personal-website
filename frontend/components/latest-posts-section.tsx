8f8a730fe32de09c5b005c2f1b683901dd08cae2
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-16 00:03:10 +0000

    Update LatestPostsSection

--- frontend/components/latest-posts-section.tsx
+++ frontend/components/latest-posts-section.tsx
@@ -1,16 +1,23 @@
 import Title from "@/ui/title";
 import LatestPosts from './latest-posts';
 
-async function LatestPostsSection() {
+// TODO: This section should not render if data fetch fails
+const LatestPostsSection = () => {
 
     return (
-      <section className='dark:bg-dark  px-6 py-16'>
-        <div className="max-w-[720px] m-auto">
+      <section 
+        className='dark:bg-dark  px-6 py-16'
+        >
+        <div 
+          className="max-w-[720px] m-auto"
+          >
           <Title>
               Latest posts
             </Title> 
             
-          <div className="flex flex-col items-center mt-8">
+          <div 
+            className="flex flex-col items-center mt-8"
+            >
             <LatestPosts /> 
           </div>
 
