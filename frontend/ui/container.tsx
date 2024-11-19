31f41bd30ee1accaddf253f341b94bfef238dbd6
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-19 19:11:29 +0000

    Add optional className Prop to overwrite default width

--- frontend/ui/container.tsx
+++ frontend/ui/container.tsx
@@ -1,8 +1,22 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-const Container = ({children}: {children: ReactNode}) => {
+type ContainerProps = {
+    children: ReactNode,
+    className?: string,
+}
+
+/**
+ * This Container component has a default max width size of 1120px.
+ * Overwrite the size using talwind max-w-[value], if desired.
+ * @param {string} [className] - An optional className prop to apply custom widths.
+ * @returns {JSX.Element} - Rendered container component.
+ */
+const Container = ({children, className}: ContainerProps): JSX.Element => {
     return (
-        <div className="max-w-[1120px] m-auto">
+        <div 
+            className={twMerge("max-w-[1120px] m-auto", className)}
+            >
             {children}
         </div>
     );
