b2e933e3bf682b11f33b462a68d9169699d460ac
Brandon Tupiti <tupitibrandon@gmail.com>
2024-11-19 19:58:15 +0000

    Update Footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -2,12 +2,14 @@ import Container from "../ui/container";
 import TextXS from "../ui/text-xs";
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+
     return ( 
         <footer className="dark:bg-dark bg-[#fffefe]">
             <Container className="max-w-[720px]">
                 <div className="flex flex-col lg:flex-row lg:justify-between gap-4 items-center pt-16 py-12 pb-8">
                     <TextXS>
-                        All rights reserved. &copy;  Brandon Tupiti 2023
+                        All rights reserved. &copy;  Brandon Tupiti {year}
                     </TextXS>
                     <TextXS>
                         Powered by AWS Fargate
