30ebe76d947d0db7fb26dada205afe220a1f1e69
Brandon Tupiti <tupitibrandon@gmail.com>
2025-01-29 04:33:18 +0000

    Update config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: process.env.MEDIA_HOSTNAME,
+          port: process.env.MEDIA_PORT,
+          pathname: '/**', 
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
 
-module.exports = nextConfig
+  
