c98efc17d94089a7962e9b282538e38e106e90c5
Brandon Tupiti <tupitibrandon@gmail.com>
2025-02-06 13:31:28 +0000

    Update next conf, remove remote image patterns

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: process.env.MEDIA_HOSTNAME,
-          port: process.env.MEDIA_PORT,
-          pathname: '/**', 
-        },
-      ],
-    },
     output: 'standalone',
   };
   
   module.exports = nextConfig;
-
   
