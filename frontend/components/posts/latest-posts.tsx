8bef0096d9e53acdabadd82ca9f7e2b268940846
Brandon Tupiti <tupitibrandon@gmail.com>
2025-04-14 07:11:24 +0100

    A totally bad commit, but I want to get it to a pipeline before I fix my errors (LOL).

--- frontend/components/posts/latest-posts.tsx
+++ frontend/components/posts/latest-posts.tsx
@@ -4,6 +4,7 @@ import PostItem from "./post-item";
 import { getLatestPostsGQL } from "@/gql-lib/queries";
 import { gqlFetch } from "@/gql-lib/gql-fetch";
 import Title from "@/ui/title";
+import { Suspense } from "react";
 
 type Post = {
     id: number;
@@ -46,7 +47,7 @@ async function LatestPosts(){
               Latest posts
           </Title> 
 
-          
+          <Suspense fallback={<div>fall back</div>}>
           <div 
               className="flex flex-col items-center mt-8"
               >
@@ -59,7 +60,7 @@ async function LatestPosts(){
               />))
           }
           </div>
-          
+          </Suspense>
 
         </div>
   );
