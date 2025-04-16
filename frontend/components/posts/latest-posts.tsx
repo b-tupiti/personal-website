fa663d68922ca69a6871f176f57701e6d646dcdf
Brandon Tupiti <tupitibrandon@gmail.com>
2025-04-16 07:18:04 +0100

    Update file.

--- frontend/components/posts/latest-posts.tsx
+++ frontend/components/posts/latest-posts.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import PostItem from "./post-item";
 import { getLatestPostsGQL } from "@/gql-lib/queries";
 import { gqlFetch } from "@/gql-lib/gql-fetch";
@@ -9,15 +11,30 @@ type Post = {
     title: string;
 }
 
+/**
+   * This component is rendered at build time (part of app/page.tsx, SSG)
+   * so the gqlFetch() call will return an error if the backend service is down.
+   * In addition to this, new posts created on the backend will not be reflected
+   * on this page, unless a rerender occurs. 
+   */
+
 async function LatestPosts(){
 
   const query = getLatestPostsGQL();
   const obj = await gqlFetch(query);
 
   if(obj instanceof Error){
-    return null;
+    return (
+      <div>
+        Failed fetch blogs because the backend service was not available when this page was generated.
+      </div>
+    );
   }
   
+  if(obj === undefined){
+    return null;
+  }
+
   let postItems = obj.data.data.pages;
     
   return ( 
@@ -29,6 +46,7 @@ async function LatestPosts(){
               Latest posts
           </Title> 
 
+          
           <div 
               className="flex flex-col items-center mt-8"
               >
@@ -41,6 +59,8 @@ async function LatestPosts(){
               />))
           }
           </div>
+          
+
         </div>
   );
 }
